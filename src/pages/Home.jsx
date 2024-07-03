f6ecd6af9e9d1f86fb47d4d88520a1d531558597
sant32 <santoshsakre21@gamil.com>
2024-07-03 18:52:55 +0530

    d1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
+import HeroSection from "../components/HeroSection"
 
 
 
 const Home = () => {
   return (
-    <>
+    <div className="mt-36">
+    <HeroSection />
     
-    <div>Home</div>
 
-    </>
+    </div>
     
   )
 }
